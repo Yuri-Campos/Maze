49844e1d42a67f99a8be45b9eb36fc66a3cdf525
Yuri Campos <ygsoares20@gmail.com>
2023-01-02 19:07:51 -0300

    create screen buffer

--- Maze.cpp
+++ Maze.cpp
@@ -9,7 +9,15 @@ int nScreenHeight = 40;
 
 int main()
 {
-   
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	
+
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 
 
 
